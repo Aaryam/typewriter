dd8d25055d37c0ddacf34ded51fdc13955b72429
BuildTools <unconfigured@null.spigotmc.org>
2021-09-14 18:55:36 +0530

    Tweaks

--- main.js
+++ main.js
@@ -1,153 +1,160 @@
-var textAreaString = document.getElementById('words-input');
+
+/* ELEMENTS */
+
+var textArea = document.getElementById('words-input');
 var wordCounter = document.getElementById('word-count');
 var charCounter = document.getElementById('char-count');
-var mathResult = document.getElementById('math-result');
+var instanceCounter = document.getElementById('instance-count');
 var contentBox = document.getElementById('contentbox');
 
-var isDrawn = true;
+/* ELEMENTS */
 
 var lastAudioElement = null;
-
 var selectedText = "";
+var isShifting = false;
 
-document.body.onload = function () {
-    renderText();
-    getCharCount();
-    getWordCount();
-};
+/* UTILITY FUNCTIONS */
 
-function getWordCount () {
+function getWordCount(string) {
     let res = [];
-    let str = textAreaString.innerText.replace(/[\t\n\r\.\?\!]/gm, " ").split(" ");
+    let str = string.replace(/[\t\n\r\.\?\!]/gm, " ").split(" ");
     str.map((s) => {
-      let trimStr = s.trim();
-      if (trimStr.length > 0) {
-        res.push(trimStr);
-      }
+        let trimStr = s.trim();
+        if (trimStr.length > 0) {
+            res.push(trimStr);
+        }
     });
 
     let wordCount = res.length;
 
-    if (textAreaString.innerText == "") {
+    if (string == "") {
         wordCount = 0;
     }
 
-    wordCounter.innerText = "Word Count: " + wordCount.toString();
+    return wordCount;
 }
 
-function setMathSelected () {
-    if (!(typeof eval(selectedText) === "undefined")) {
-        mathResult.innerText = "Math: " + eval(selectedText);
+function getInstanceCount(string, word) {
+    let res = [];
+    let str = string.replace(/[\t\n\r\.\?\!]/gm, " ").split(" ");
+    str.map((s) => {
+        let trimStr = s.trim();
+        if (trimStr.length > 0 && trimStr.toLowerCase() == word.toLowerCase()) {
+            res.push(trimStr);
+        }
+    });
+
+    let wordCount = res.length;
+
+    if (string == "") {
+        wordCount = 0;
     }
-    else {
-        mathResult.innerText = "Math: " + "None selected";
-    }
+
+    return wordCount;
 }
 
-function getCharCount () {
-    let charCount = textAreaString.innerText.length;
+function getCharCount(string) {
+    let charCount = string.length;
 
-    if (textAreaString.innerText == "") {
+    if (string == "") {
         charCount = 0;
     }
 
-    charCounter.innerText = "Character Count: " + charCount.toString();
+    return charCount;
 }
 
-function saveText (txt) {
+function saveText(txt) {
     localStorage.setItem("textContent", txt);
 }
 
-function renderText () {
-    textAreaString.innerText = localStorage.getItem("textContent");
+function renderText() {
+    textArea.innerText = localStorage.getItem("textContent");
 }
 
-function createAudio (isOther) {
-    let rand = Math.round(Math.random() * 3);
-
-    if (isOther == null || isOther == false) {
-        if (rand == 0) {
-            playSound('assets/audio/sound.wav');
-        }
-        else if (rand == 1) {
-            playSound('assets/audio/sound.wav');
-        }
-        else if (rand == 2) {
-            playSound('assets/audio/sound.wav');
-        }
-        else {
-            playSound('assets/audio/sound.wav');
-        }
-    }
-}
-
-function playSound (audioSrc, rand) {
+function playSound(audioSrc, volume) {
     let audio = document.createElement('audio');
     audio.src = audioSrc;
     audio.autoplay = true;
-    audio.volume = 0.2;
-    if (rand != null) {
-        audio.volume = Math.fround(Math.random(5));
-        audio.playbackRate = rand;
-    }
-    document.body.appendChild(audio);
-    setTimeout(function() { audio.remove() }, 300);
-}
-
-function getWordStats (word) {
-    
-}
-
-function toggleDrawer () {
-    if (isDrawn) {
-        textAreaString.style.minWidth = "calc(40vw - 80px)";
-        contentBox.style.minWidth = "calc(60vw - 80px)";
+    if (volume != null) {
+        audio.volume = volume;
     }
     else {
-        textAreaString.style.minWidth = "calc(70vw - 80px)";
-        contentBox.style.minWidth = "calc(30vw - 80px)";
+        audio.volume = 0.2;
     }
-    isDrawn = !isDrawn;
+    document.body.appendChild(audio);
+    setTimeout(function () { audio.remove() }, 300);
 }
 
-textAreaString.addEventListener('input', function (e) {
-    getWordCount();
-    createAudio();
-    getCharCount();
-    saveText(textAreaString.innerText);
+function getSelectedText() {
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        text = window.getSelection().toString();
+    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+/* UTILITY FUNCTIONS */
+
+/* OTHER FUNCTIONS */
+
+function setText(variableName, str, value) {
+    variableName.innerText = str + value.toString();
+}
+
+function updateCounts(txt) {
+    setText(wordCounter, "Words: ", getWordCount(txt));
+    setText(charCounter, "Characters: ", getCharCount(txt));
+    setText(instanceCounter, "Instances: ", getInstanceCount(textArea.innerText, getSelectedText()));
+}
+
+/* OTHER FUNCTIONS */
+
+/* EVENT LISTENERS */
+
+document.body.onload = function () {
+    renderText();
+    updateCounts(textArea.innerText);
+};
+
+textArea.addEventListener('input', function (e) {
+    updateCounts(textArea.innerText);
+    saveText(textArea.innerText);
+    
+    if (e.data != null) {
+        playSound('assets/audio/sound.wav');
+    }
 });
 
-textAreaString.addEventListener('keydown', function (e) {
+textArea.addEventListener('keydown', function (e) {
     if (e.keyCode == 13) {
         playSound('assets/audio/enter-sound.wav');
     }
     else if (e.keyCode == 8 || e.keyCode == 46) {
         playSound('assets/audio/delete-sound.wav');
+        instanceCounter.innerText = "Instances: " + "None";
     }
 });
 
 document.addEventListener('selectionchange', function (e) {
-    selectedText = window.getSelection().toString();;
-    setMathSelected();
+    selectedText = getSelectedText();
 
-    if (selectedText != "") {
+    if (selectedText != "" && selectedText != null && selectedText != undefined) {
         let res = [];
         let str = selectedText.replace(/[\t\n\r\.\?\!]/gm, " ").split(" ");
         str.map((s) => {
-          let trimStr = s.trim();
-          if (trimStr.length > 0) {
-            res.push(trimStr);
-          }
+            let trimStr = s.trim();
+            if (trimStr.length > 0) {
+                res.push(trimStr);
+            }
         });
-    
-        let wordCount = res.length;
-        wordCounter.innerText = "Word Count: " + res.length;
-        charCounter.innerText = "Character Count: " + selectedText.toString().length;
+
+        wordCounter.innerText = "Words: " + res.length;
+        charCounter.innerText = "Characters: " + selectedText.toString().length;
+
+        updateCounts(getSelectedText());
     }
 });
-
-contentBox.addEventListener('dblclick', function (e) {
-    toggleDrawer();
-})
 
 // git push -u origin main
